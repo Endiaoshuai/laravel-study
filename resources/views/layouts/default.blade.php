79669094a9b9b8d6e7b2f013c612a03938800e61
Huangzhaoshuai <455960162@qq.com>
2019-12-20 14:26:01 +0800

    基础页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>@yield('title', 'Weibo App') - Laravel 新手入门</title>
+        <title>@yield('title', 'Weibo App') - Laravel 入门</title>
     </head>
     <body>
         @yield('content')
