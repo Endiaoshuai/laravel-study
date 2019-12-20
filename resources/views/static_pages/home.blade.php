db41485d0692ecc26c3bb3b8ae3581a923d939c5
Huangzhaoshuai <455960162@qq.com>
2019-12-20 16:18:36 +0800

    样式初始化

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 <!-- 使用 '@ section' 和 '@ stop' 代码来填充父视图的 content 区块 -->
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="">Laravel 入门教程</a> 的实例主页。
+        </p>
+        <p>
+            <a  class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
