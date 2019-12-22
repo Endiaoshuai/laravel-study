695b4eb430c5466d70f8ecb87b6448f3909bcd90
endiaoshuai <455960162@qq.com>
2019-12-22 22:34:45 +0800

    用戶model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60); // 最大长度 60
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); // 这个方法会创建 created_at updated_at 两个字段
         });
     }
 
