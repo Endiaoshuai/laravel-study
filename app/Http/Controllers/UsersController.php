cb2c5b61f056b1fb7e5c72e3308f5630fde37e6d
endiaoshuai <455960162@qq.com>
2019-12-23 00:56:27 +0800

    mysql 版本

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    // show 方法声明类型 Eloquent 模型 User
+    public function show(User $user)
+    {
+        // 通过 compact 方法转化为一个关联数组
+        var_dump(compact('user'));
+        return view('users.show', compact('user'));
     }
 }
