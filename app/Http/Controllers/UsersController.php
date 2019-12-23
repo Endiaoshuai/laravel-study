803aa8beb16f236eabdb071affd005696cb817b7
Huangzhaoshuai <455960162@qq.com>
2019-12-23 05:13:54 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,9 +25,20 @@ class UsersController extends Controller
         // 数据校验， 由 App\Http\Controllers\Controller 类中的 ValidatesRequests 进行定义
         $this->validate($request, [
             'name' => 'required|max:50',
-            'emai' => 'required|email|unique:users|max:255',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        dump(session()->get('success'));
+        // 等同于 redirect()->route('users.show', [$user->id]);
+        return redirect()->route('users.show', [$user]); // route方法会自动获取Model的主键
+        
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="row">
-        <div class="offset-md-2 clo-md-8">
+        <div class="offset-md-2 col-md-8">
             <div class="col-md-12">
                 <div class="offset-md-2 col-md-8">
                     <section class="user_info">
